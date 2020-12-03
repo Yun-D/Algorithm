6e923bd1ba6cf95f3fd6420d205e9765018d115d
Yun-D <yund_272@naver.com>
2020-12-04 01:25:18 +0900

    201204 - BOJ 2869
    스터디 후 성공~

--- #2869/Main.java
+++ #2869/Main.java
@@ -1,7 +1,6 @@
 import java.io.*;
 import java.util.*;
 
-//실패!!!!!!!!
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,16 +11,11 @@ public class Main {
 		int v = Integer.parseInt(st.nextToken());
 		
 		int move = a - b; //하루동안 이동할 수 있는 거리
-		int result = 0;
+		int result = (v-b) / move;
 		
-		if ( v == a )  //하루만에 도달 가능하다면
-			result = 1;
-		else {
-			if (v % move != 0 || v-move < a) //막대 길이 % 이동 거리가 나누어 떨어지지 않을 경우
-				result = (v-b) / move + 1;
-			else //나누어 떨어진다면
-				result = (v-b) / move; //달팽이가 도달하는 날은 미끄러지지 않음!!!
-		}
+		//만약 달팽이가 도달한 result에 나머지가 남았다면 (갈 길이 남았다면)
+		if ((v-b) % move != 0)
+			result++;
 		
 		System.out.println(result);
 	}
