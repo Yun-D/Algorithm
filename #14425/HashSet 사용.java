1d3bb1cb7c499e0f3959c2d770767c625a7b2412
Yun-D <yund_272@naver.com>
2022-05-24 14:53:54 +0900

    220524 BOJ-14425

---
+++ #14425/HashSet 사용.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {	
+	public static void main(String[] args) throws Exception, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int equalNum = 0;
+		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		Set<String> dataList = new HashSet<>(); //기준 데이터 집합
+		
+		for (int i=0; i<N; i++) {
+			dataList.add(br.readLine());
+		}
+		
+		for (int i=0; i<M; i++) {
+			if(dataList.contains(br.readLine()))
+				equalNum++;
+		}
+		
+		System.out.println(equalNum);
+	}
+}
