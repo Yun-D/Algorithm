44f4adad48f402ff8f24a984856a8aed49f31ff2
Yun-D <yund_272@naver.com>
2023-07-08 01:47:01 +0900

    230707 BOJ-4153

---
+++ BOJ/#4153/main.py
@@ -0,0 +1,10 @@
+while True :
+    abc = list(map(int, input().split()))
+    abc.sort()
+
+    if abc[0] == 0 and abc[1] == 0 and abc[2] == 0 :
+        exit()
+    elif abc[0]**2 + abc[1]**2 == abc[2]**2 :
+        print("right")
+    else:
+        print("wrong")
